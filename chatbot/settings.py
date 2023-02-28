0631e29bb37733881acfb862c4d097c0adb23793
yusuf <yachour31@gmail.com>
2023-02-28 03:18:17 +0200

    disable csrf

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -132,6 +132,7 @@ STATIC_DIR = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_COOKIE_SECURE = False
 
 
 openai.api_key = config("OPENAI_API_KEY")
